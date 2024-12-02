8025b871495edb673d5bb6ef735c1d91e797bc17
Ghazanfar <ghazanfar.ali.0347@gmail.com>
2024-12-03 00:21:20 +0100

    (FEAT): draw board function implemented

--- main.cpp
+++ main.cpp
@@ -9,11 +9,22 @@ bool checkTie(char *spaces);
 
 int main(){
     std::cout<<"Tic Tac Toe Game";
+    char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    drawBoard(spaces);
     return 0;
 }
 
 
 void drawBoard(char *spaces){
+    std::cout<<"\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  "<<spaces[0]<<"  |  "<<spaces[1]<<"  |  "<<spaces[2]<<"  " << "\n";
+    std::cout << "_____|_____|_____" << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  "<<spaces[3]<<"  |  "<<spaces[4]<<"  |  "<<spaces[5]<<"  " << "\n";
+    std::cout << "_____|_____|_____" << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  "<<spaces[6]<<"  |  "<<spaces[7]<<"  |  "<<spaces[8]<<"  " << "\n";
 
 }
 void playerMove(char *spaces, char player){
