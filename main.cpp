bd649344f99f347e423473045e5ae82fd671ec93
Ghazanfar <ghazanfar.ali.0347@gmail.com>
2024-12-03 00:07:31 +0100

    (Feat)-Initial Setup

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+int main(){
+    std::cout<<"Tic Tac Toe Game";
+    return 0;
+}
+
+
+void drawBoard(char *spaces){
+
+}
+void playerMove(char *spaces, char player){
+
+}
+void computerMove(char *spaces, char computer){
+
+}
+bool checkWinner(char *spaces, char player, char computer){
+    return 0;
+}
+bool checkTie(char *spaces){
+    return 0;
+}
