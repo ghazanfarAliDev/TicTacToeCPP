41475fe2c8247c671f8de0426a858b5497b9cd49
Ghazanfar <ghazanfar.ali.0347@gmail.com>
2024-12-03 14:30:23 +0100

    (FEAT): Implementation of player and computer moves

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<cstdlib>
 #include<ctime>
+
 
 void drawBoard(char *spaces);
 void playerMove(char *spaces, char player);
@@ -10,7 +12,16 @@ bool checkTie(char *spaces);
 int main(){
     std::cout<<"Tic Tac Toe Game";
     char spaces[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
     drawBoard(spaces);
+    while (running){
+        playerMove(spaces, player);
+        drawBoard(spaces);
+        computerMove(spaces, computer);
+        drawBoard(spaces);
+    }
     return 0;
 }
 
@@ -28,10 +39,28 @@ void drawBoard(char *spaces){
 
 }
 void playerMove(char *spaces, char player){
+    int number;
+    do{
+        std::cout<<"Enter a spot to place a marker (1-9)";
+        std::cin >> number;
+        number--;
+        if(spaces[number] == ' '){
+            spaces[number] = player;
+            break;
+        }
+    }while(!number > 0 || !number < 8);
 
 }
 void computerMove(char *spaces, char computer){
-
+    int number;
+    srand(time(0));
+    while (true){
+        number = rand() % 9;
+        if (spaces[number] == ' '){
+            spaces[number]= computer;
+            break;
+        }
+    }
 }
 bool checkWinner(char *spaces, char player, char computer){
     return 0;
